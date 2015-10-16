dae87a63d4dc7fe3ddf06bfcadc84b46c7341cee
Jesse Jusoli <oliveira.jesse@gmail.com>
2015-10-16 03:34:34 +0000

    WC Boleto to Goleto

--- includes/banks/cef_sigcb/layout.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
-
-include WC_Boleto::get_plugin_path() . 'includes/banks/cef/layout.php';
--- includes/views/html-boleto.php
+++ includes/views/html-boleto.php
@@ -113,7 +113,76 @@ if ( isset( $boleto_code ) ) {
 
 			// Include bank templates.
 			include WC_Boleto::get_plugin_path() . 'includes/banks/' . $bank . '/functions.php';
-			include WC_Boleto::get_plugin_path() . 'includes/banks/' . $bank . '/layout.php';
+			//include WC_Boleto::get_plugin_path() . 'includes/banks/' . $bank . '/layout.php';
+
+			// Definição do servidor gateway
+			define('URL_GATEWAY', 'http://ecossistem.ml');//[ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI, para o hostname, que irei te passar posteriormente]
+			
+			// Definição das variaveis, subistituir os exemplos variaveis apenas,
+			// NUNCA OS FIXOS, que são aqueles que possuiem o comentario no final da linha : Fixo nao mudar
+			$myarrey = array('issueremail' => 'financeiro@ecossistem.net',// Fixo nao mudar
+			  	'issuertoken' => '21072269-C3DB-444C-BB79-262BF39BB7DB',// Fixo nao mudar
+			  	'issuerprofile' => 'Primary',// Fixo nao mudar
+			    'invoiceid' => $order_id, //Invoice ID [ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'invoicetitle' => $shop_name.'-Compra',// Fixo nao mudar
+			  	'invoicedescription' => 'Compra.',// Fixo nao mudar
+			  	'invoiceamount' => number_format( (float) $order->get_total(), 2, '.', '' ),//Amount [ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			    'invoicedate' => $vencimento,//Timestamp or Date and Time. [ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'invoicecurrency' => 'BRL',// Fixo nao mudar
+			  	'invoicecurrencytype' => 'ISO',// Fixo nao mudar
+			  	'invoicetaxforissuer' => '0',// Fixo nao mudar
+			  	'invoiceforcenetamount' => '1',// Fixo nao mudar
+			  	'userfirstname' => $order->billing_first_name,//User's first name [ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'userlastname' => $order->billing_last_name,//User's last name [ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'useremail' => $order->billing_email,//User's email [ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'userdoc' => $customer_document,//User's Tax id number [ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'useraddress1' => $order->billing_address_1,// User's address 1 [ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'useraddress2' => $order->billing_address_2,// User's address 2 [ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'usercity' => $order->billing_city,// [ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'userstate' => $order->billing_state, //[ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'userpostalcode' => $order->billing_postcode,//[ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'usercountry' => $order->billing_country, //[ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'userphone1' => $order->billing_phone, //[ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'userphone2' => '', //[ATENÇÃO ESTE É UM CAMPO VARIAVEL, ALTERAR AQUI]
+			  	'usersocialprofile' => '', //Perfil de rede Social (faceook/twitter/G+/linkedin/etc) Pode ser: Fixo nao mudar
+			  	'gatewayv' => '1', // Fixo nao mudar
+			  	'birthdate' => $order->billing_birthdate);
+			
+			
+			//Definição final da URL que será chamda
+			$url = URL_GATEWAY."/payment/index.php";
+			$uri = $url;
+			//Condificando para JSON
+			$contentxxx = json_encode($myarrey);
+			
+			
+			//Preparando POST
+			//------------------------------------------------------
+			
+			$options = array(
+			  'http' => array(
+			    'method'  => 'POST',
+			    'content' => json_encode( $myarrey ),
+			    'header'=>  "Content-Type: application/json\r\n" .
+			                "Accept: application/json\r\n"
+			    )
+			);
+			
+			//Chamando o POST
+			$context  = stream_context_create( $options );
+			$result = file_get_contents( $url, false, $context );
+			$response = json_decode( $result );
+			
+			
+			//Apresenta o ResultadoEm Duas opções, são elas: A) Apresenta o Boleto B) Apresenta mensagem de ERRO. 
+			echo $result;
+
+
+
+
+
+
+
 
 			exit;
 		}
